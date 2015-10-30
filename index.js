f3e77b429612f26edc175d95b52c078af05f7e88
Rafaell Lycan <sonny.webdsg@gmail.com>
2015-10-30 17:33:12 -0200

    Add ejs and passport

--- index.js
+++ index.js
@@ -3,24 +3,28 @@
 let path = require('path');
 let express = require('express');
 let bodyParser = require('body-parser');
-
-let routes = require('./routes/routes');
+let passport = require('passport');
+let session = require('express-session');
 
 let app = express();
 
+//View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(session({secret: 'secret123'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', function (req, res) {
-  res.send('<h1>Will Render our APP</h1>');
+  res.render('index');
 });
 
 //API Routes
-app.use('/api', routes);
+app.use('/api', require('./routes/routes'));
 
 //Catch 404 routes
 app.use(function (req, res, next) {
